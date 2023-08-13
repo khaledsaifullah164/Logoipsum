67cacc9dedb540facf332856d1b489b01adf3341
khaledsaifullah164 <kmsaifullah9@gmail.com>
2023-08-14 01:50:06 +0600

    project done

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(".card-wrapper").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+$(".clients-wrapper").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
